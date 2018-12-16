533a09bc14b03ce2c6a768e0f243462185dfc621
ElodKuthy <kuthyelod@gmail.com>
2018-12-16 17:25:57 +0100

    Add material UI

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
 import React from 'react';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import { createMuiTheme } from '@material-ui/core/styles';
+import Typography from '@material-ui/core/Typography';
+import { ThemeProvider } from '@material-ui/styles';
+
+const theme = createMuiTheme({
+  typography: {
+    useNextVariants: true,
+  },
+});
 
 function App() {
   return (
-    <h1>Kotei web app</h1>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Typography variant="h1" align="center" gutterBottom>
+        Kotei web app
+      </Typography>
+    </ThemeProvider>
   )
 }
 
